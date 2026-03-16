eeadd923ac15b627f6b1dfed00433aff9d4f549e
Jake Soenneker <jake@soenneker.com>
2026-03-16 10:58:55 -0500

    misc

--- src/Soenneker.Nws.HttpClients/Abstract/INwsOpenApiHttpClient.cs
+++ src/Soenneker.Nws.HttpClients/Abstract/INwsOpenApiHttpClient.cs
@@ -8,7 +8,13 @@ namespace Soenneker.Nws.HttpClients.Abstract;
 /// <summary>
 /// A .NET thread-safe singleton HttpClient for 
 /// </summary>
-public interface INwsOpenApiHttpClient: IDisposable, IAsyncDisposable
+public interface INwsOpenApiHttpClient : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Asynchronously retrieves an instance of <see cref="HttpClient"/> for making HTTP requests.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous operation.</param>
+    /// <returns>A <see cref="ValueTask{TResult}"/> representing the asynchronous operation. The result contains an <see
+    /// cref="HttpClient"/> instance for sending HTTP requests.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
